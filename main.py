10ebaf61e207f132233094f146ae99dabee90ba8
Kishore Dama <kishoredama369@gmail.com>
2023-08-23 12:43:38 +0530

    added invoice date

--- main.py
+++ main.py
@@ -9,10 +9,17 @@ print(filepaths)
 
 for filepath in filepaths:
     df = pd.read_excel(filepath,sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit='mm',format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, invoice_dt  = filename.split("-")
+
     pdf.set_font(family="Times",size=16,style="B")
-    pdf.cell(w=50, h=8, txt=invoice_nr)
+    pdf.cell(w=50, h=8, txt=f"invoice_nr{invoice_nr}", ln=1) # ln is to create break line
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {invoice_dt}")
+
     pdf.output(f"PDFs/{filename}.pdf")
